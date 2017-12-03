c752cd19310a2b788ca50896600ab82ad498082b
yu.lu <yu.lu@jyall.com>
2017-12-03 17:32:48 +0800

    提交代码

---
+++ newsManager/src/main/java/com/yulu/mangger/constants/AddressConstants.java
@@ -0,0 +1,13 @@
+package com.yulu.mangger.constants;
+
+public class AddressConstants {
+
+    public interface News {
+        String NEWS = "news";
+        String NEWS_LIST = "list";
+        interface API{
+            String NEWS = "news";
+            String NEWS_LIST = "list";
+        }
+    }
+}
